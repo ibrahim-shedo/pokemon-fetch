3d9b76d26dd005c83b34f48f8afa0721e737b62a
ibrahim-shedo <135816154+ibrahim-shedo@users.noreply.github.com>
2024-02-10 16:52:23 +0300

    Add files via upload

---
+++ pokemon.fetch/script.js
@@ -0,0 +1,18 @@
+async function fetchData(){
+    try {
+        const pokemonName = document.getElementById("pokemonNames").value.toLowerCase(); 
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        if (!response.ok) {
+            throw new Error("Failed to fetch data from PokeAPI");
+        }
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById('pokemonSprite');
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
